8b70d1d04921f182924fa4394ae5894a239cfcf3
Michael Muzyka <MMuzyka@verismic.com>
2019-08-14 15:51:11 +1000

    add simple ASP.NET Core 2.2 app with one Api and Health Checks

---
+++ Tringo.WebApp/HealthChecks/MainHealthCheck.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tringo.WebApp.HealthChecks
+{
+    public class MainHealthCheck : IHealthCheck
+    {
+        public MainHealthCheck()
+        {
+
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            // Execute health check logic here. This example sets a dummy
+            // variable to true.
+            var healthCheckResultHealthy = true;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("The check indicates a healthy result."));
+            }
+
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("The check indicates an unhealthy result."));
+        }
+    }
+}
---
+++ Tringo.WebApp/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Tringo.WebApp.HealthChecks;
+
+namespace Tringo.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //Health Checks
+            services.AddHealthChecks()
+                .AddCheck<MainHealthCheck>(
+                    nameof(MainHealthCheck),
+                    failureStatus: HealthStatus.Degraded,
+                    tags: new[] { "initial" });
+
+            // Polly. Will be set up later
+            services.AddHttpClient(); 
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = (check) => check.Tags.Contains("initial"),
+                AllowCachingResponses = false,
+            });
+        }
+    }
+}
