ecb1a5a975aa5973af15c90cd8eb59a8b40e3ef2
Michael Muzyka <MMuzyka@verismic.com>
2019-08-16 22:08:54 +1000

    add docker

--- Tringo.WebApp/Startup.cs
+++ Tringo.WebApp/Startup.cs
@@ -72,10 +72,10 @@ namespace Tringo.WebApp
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
 
             app.UseHealthChecks("/health", new HealthCheckOptions()
