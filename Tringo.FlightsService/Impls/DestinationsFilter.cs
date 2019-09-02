ba40ed283427490c93e4e2941e02872eac2b5382
Michael Muzyka <MMuzyka@verismic.com>
2019-09-03 00:21:38 +1000

    make duration enum consistent with UI enum

--- Tringo.FlightsService/Impls/DestinationsFilter.cs
+++ Tringo.FlightsService/Impls/DestinationsFilter.cs
@@ -66,7 +66,7 @@ namespace Tringo.FlightsService.Impls
 							&& f.DateDeparture.DayOfWeek == DayOfWeek.Saturday);
 						break;
 					default:
-						break;
+						throw new ArgumentException("Unknown dates specified, but duration is not set'");
 				}
 
 				return flights;
