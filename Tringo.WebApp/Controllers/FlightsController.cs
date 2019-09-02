7575654c117e33ecf73434f9aaf9229b7f566b83
Michael Muzyka <MMuzyka@verismic.com>
2019-09-03 00:54:04 +1000

    do not filter by budget if it's not set

--- Tringo.WebApp/Controllers/FlightsController.cs
+++ Tringo.WebApp/Controllers/FlightsController.cs
@@ -45,15 +45,17 @@ namespace Tringo.WebApp.Controllers
                 .ToList();
             var airportsIatas = relatedAirports.Select(a => a.IataCode);
 
-            // Base filtering:
-            // - by Budget
-            // - by flights to airports within requested area
-            var filteredFlights = allFlights
-                .Where(f =>
-                    inputData.Budget.Min < f.LowestPrice && f.LowestPrice < inputData.Budget.Max
-                    && airportsIatas.Contains(f.To))
-                .ToList();
+			// Filtering:
+			// Filter by flights to airports within requested area
+			var filteredFlights = allFlights.Where(f => airportsIatas.Contains(f.To)).ToList();
 
+			// Filter by Budget
+			if (inputData.Budget != null)
+			{
+				filteredFlights = filteredFlights.Where(f =>
+					inputData.Budget.Min < f.LowestPrice && f.LowestPrice < inputData.Budget.Max).ToList();
+			}
+			
             // Filter by Dates
             filteredFlights = _destinationsFilter
                 .FilterFlightsByDates(filteredFlights, inputData.Dates)
