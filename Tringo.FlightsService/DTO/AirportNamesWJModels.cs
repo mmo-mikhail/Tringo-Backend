db5561d9f84f2ee1c789677592a344ad30400dec
Michael Muzyka <MMuzyka@verismic.com>
2019-09-18 23:39:06 +1000

    add mock files from WJ

---
+++ Tringo.FlightsService/DTO/AirportNamesWJModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tringo.FlightsService.DTO
+{
+    public class AirportNamesWJModels
+    {
+        public List<AirportCityInfoModel> AirportCityInfo { get; set; }
+    }
+
+    public class AirportCityInfoModel
+    {
+        public string TSAAirportCode { get; set; }
+        public string AirportName { get; set; }
+        public string CityCode { get; set; }
+        public string CityName { get; set; }
+        public string CountryCode { get; set; }
+    }
+}
