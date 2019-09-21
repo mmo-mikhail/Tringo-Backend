0a97b0132b5ccc0d2305f4f1840697bc549c37c2
Michael Muzyka <MMuzyka@verismic.com>
2019-09-21 21:06:25 +1000

    add Airports Controller

---
+++ Tringo.WebApp.Tests/AirportsControllerTest.cs
@@ -0,0 +1,95 @@
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tringo.FlightsService;
+using Tringo.FlightsService.DTO;
+using Tringo.WebApp.Controllers;
+using Xunit;
+
+namespace Tringo.WebApp.Tests
+{
+    public class AirportsControllerTest
+    {
+        [Theory]
+        [InlineAutoMoqData(null)]
+        [InlineAutoMoqData("")]
+        [InlineAutoMoqData("   ")]
+        public void GetDestinationPrices_BadRequest(
+            string airportCode,
+            AirportsController airportsController
+        )
+        {
+            // Act
+            var result = airportsController.GetAirportCoordinates(airportCode);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Value.Should().BeNull();
+            result.Result.Should().BeOfType<BadRequestResult>();
+            ((StatusCodeResult)result.Result).StatusCode.Should().Be(400);
+        }
+
+        [Theory]
+        [InlineAutoMoqData("ADL")]
+        public void GetDestinationPrices_NotFound(
+            string airportCode,
+            [Frozen] Mock<IAirportsService> airportsService,
+            AirportsController airportsController)
+        {
+            // Setup: airports in backend: SYD, MEL. request coming for ADL (adelaide) which is missing
+            // Arrange
+            var airports = new List<AirportDto>()
+            {
+                new AirportDto(){ IataCode = "SYD" },
+                new AirportDto(){ IataCode = "MEL" }
+            };
+            airportsService.Setup(_ => _.GetAirports()).Returns(airports);
+
+            // Act
+            var result = airportsController.GetAirportCoordinates(airportCode);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<NoContentResult>();
+            ((NoContentResult)result.Result).StatusCode.Should().Be(204);
+        }
+
+        [Theory]
+        [InlineAutoMoqData("SYD")]
+        public void GetDestinationPrices_Found_Success(
+            string airportCode,
+            double expectedLat,
+            double expectedLng,
+            [Frozen] Mock<IAirportsService> airportsService,
+            AirportsController airportsController)
+        {
+            // Setup: airports in backend: SYD, MEL. request coming for SYD (adelaide) which exists
+            // Arrange
+            var airports = new List<AirportDto>()
+            {
+                new AirportDto(){ IataCode = "SYD", Lat = expectedLat, Lng = expectedLng },
+                new AirportDto(){ IataCode = "MEL" }
+            };
+            airportsService.Setup(_ => _.GetAirports()).Returns(airports);
+
+            // Act
+            var result = airportsController.GetAirportCoordinates(airportCode);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().BeOfType<OkObjectResult>();
+            ((OkObjectResult)result.Result).StatusCode.Should().Be(200);
+
+            var okResultObj = ((OkObjectResult)result.Result).Value;
+            okResultObj.Should().BeOfType<Coordinates>();
+
+            var resCoord = okResultObj as Coordinates;
+            resCoord.Lat.Should().Be(expectedLat);
+            resCoord.Lng.Should().Be(expectedLng);
+        }
+    }
+}
---
+++ Tringo.WebApp/Controllers/AirportsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tringo.FlightsService;
+using Tringo.FlightsService.DTO;
+using Tringo.WebApp.Models;
+
+namespace Tringo.WebApp.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    public class AirportsController : ControllerBase
+    {
+        private readonly IAirportsService _airportsService;
+
+        public AirportsController(IAirportsService airportsService)
+        {
+            _airportsService = airportsService;
+        }
+
+        [HttpGet]
+        public ActionResult<Coordinates> GetAirportCoordinates(string airportCode)
+        {
+            if (string.IsNullOrWhiteSpace(airportCode))
+                return new BadRequestResult();
+
+            var airport = _airportsService.GetAirports().FirstOrDefault(a => a.IataCode == airportCode);
+            return airport != null
+                ? Ok(new Coordinates(airport.Lat, airport.Lng))
+                : NoContent() as ActionResult;
+        }
+    }
+}
